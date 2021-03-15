a2c1c75c972b42497187cbd2b2e1098ee19781e2
Andrea <ac.ramirez11@uniandes.edu.co>
2021-03-15 11:37:14 -0500

    Web_app with algoritmica of objects

---
+++ app.py
@@ -0,0 +1,33 @@
+from models.item import Item
+
+
+url = "https://www.johnlewis.com/2019-apple-ipad-mini-apple-a12-ios-7-9-inch-wi-fi-64gb/space-grey/p4067399"
+tag_name = "p"
+query = {"class": "price price--large"}
+
+item= Item(url,tag_name,query)
+print(item.load_price())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
